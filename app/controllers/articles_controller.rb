16742416af09f630cf78167abb6618357fa2fb97
Muhib Hasan s3850034 <s3850034@student.rmit.edu.au>
2021-05-04 01:08:41 +1000

    Added the ability to create new object and then validate object

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,18 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
     end
 
     def create
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+            
     end
-
 end
+
